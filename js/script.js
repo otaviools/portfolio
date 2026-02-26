f46d8e38d5dba864ef910c8f399dc4c995224373
otaviools <otaviools13@gmail.com>
2026-02-26 12:37:29 -0300

    geral

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
-//Menu Mobile
-const mobileMenu = document.querySelector(".mobile-menu");
-const headerMenu = document.querySelector(".header-menu");
+//Script SideBar
+const sidebar = document.querySelector(".sideBar");
+const menuOpen = document.querySelector(".icon-menu-open");
+const menuClose = document.querySelector(".icon-menu-close");
+const sideBarLinks = document.querySelectorAll(".sideBar a");
 
-mobileMenu.addEventListener("click", () => {
-  headerMenu.classList.toggle("active");
-  mobileMenu.classList.toggle("active");
+menuOpen.addEventListener("click", function () {
+  sidebar.classList.add("active");
 });
+
+menuClose.addEventListener("click", function () {
+  sidebar.classList.remove("active");
+});
+
+sideBarLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    sidebar.classList.remove("active");
+  });
+});
+
+//Script Animação
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
