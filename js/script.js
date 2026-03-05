60cfc52b5d19c6de003ff845c6137eb440ec8109
otaviools <otaviools13@gmail.com>
2026-03-05 13:58:26 -0300

    scrolled

--- js/script.js
+++ js/script.js
@@ -13,6 +13,26 @@ if (sidebar && menuOpen && menuClose) {
   );
 }
 
+//Script Scroll Header
+const header = document.querySelector(".header");
+let lastScroll = 0;
+
+window.addEventListener(
+  "scroll",
+  () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+
+    lastScroll = currentScroll;
+  },
+  { passive: true },
+);
+
 //Script Animação Suave
 
 const observer = new IntersectionObserver((entries) => {
